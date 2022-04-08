21f28889ce4492882d44d8fa79fcecdc711494c0
Michael Silva <michaelsilv@gmail.com>
2022-04-07 21:35:17 -0300

    Criação dos arquivos etapa (JSON) e criação do script.js. Adição dos arquivos de audio, e importação do arquivo da biblioteca do Ajax HOWLER

---
+++ etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+      titulo: "VEREADOR",
+      numeros: 5,
+      candidatos: [
+        {
+          numero: "38111",
+          nome: "Son Goku",
+          partido: " sayajin",
+          fotos: [{ src: "../img/goku.jpg", legenda: "Vereador" }],
+        },
+        {
+          numero: "22222",
+          nome: "Cavaleiro da lua",
+          partido: "MARVEL",
+          fotos: [{ src: "../img/cavaleiro da lua.jpg", legenda: "Vereador" }],
+        },
+        {
+            numero: "77222",
+            nome: "Homem Aranha",
+            partido: "MARVEL",
+            fotos: [{ src: "homem aranha.jpg", legenda: "Vereador" }],
+          },
+      ],
+    },
+    {
+      titulo: "PREFEITO",
+      numeros: 2,
+      candidatos: [
+        {
+          numero: "99",
+          nome: "Macaco Tião",
+          partido: "Zoológico do Rio",
+          vice: "Prefeito",
+          fotos: [
+            { src: "../img/tiao.jpg", legenda: "Prefeito" },
+            { src: "../img/prefeito.jpg", legenda: "Vice-Prefeito", small: true },
+          ],
+        },
+        {
+          numero: "84",
+          nome: "Batman",
+          partido: "D.C",
+          vice: "Joker",
+          fotos: [
+            { src: "../img/batman.jpg", legenda: "Prefeito" },
+            { src: "../img/coringa.jpg", legenda: "Vice-Prefeito", small: true },
+          ],
+        },
+      ],
+    },
+  ];
+  
---
+++ script.js
@@ -0,0 +1,9 @@
+//Encurtador de comandos
+let qs = (el) => document.querySelector;
+
+let seuVotoPara =  document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descicao = document.querySelector('.d-1-4');
+let aviso =  document.querySelector('.d-2');
+let lateral =  document.querySelector('.d-1-right');
+let numeros =  document.querySelector('d-a-3');
