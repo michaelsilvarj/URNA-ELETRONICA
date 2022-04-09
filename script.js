eac76761e961ec2cefc7adcfb1520b093e5f81b0
Michael Silva <michaelsilv@gmail.com>
2022-04-09 00:26:04 -0300

    Correção dos bug na foto

--- script.js
+++ script.js
@@ -54,9 +54,9 @@ function atualizaInterface() {
         let fotosHtml = '';
         for(let i in candidato.fotos) {
             if(candidato.fotos[i].small) {
-                fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+                fotosHtml += `<div class="d-1-image small"><img src="img/${candidato.fotos[i].src}" alt="" />${candidato.fotos[i].legenda}</div>`;
             } else {
-                fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+                fotosHtml += `<div class="d-1-image"><img src="img/${candidato.fotos[i].src}" alt="" />${candidato.fotos[i].legenda}</div>`;
             }
         }
         lateral.innerHTML = fotosHtml;
